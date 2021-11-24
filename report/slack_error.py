54913e33ee4adc261804a1ff93f3b09de5b5d45a
Adam Shimali <adam.shimali@gmail.com>
2021-11-24 10:29:18 +0000

    Add github action to sent test failures to slack.
    Make test fail so we can check slack integration.

--- report/slack_error.py
+++ report/slack_error.py
@@ -1,6 +1,7 @@
 import requests
 import os
 
-SLACK_URL = os.environ.get("SLACK_WEBHOOK_URL")
+
+SLACK_URL = os.environ.get("SLACK_SMOKE_TEST_WEB_HOOK_URL")
 if SLACK_URL is not None:
-    requests.post(SLACK_URL, json={"text":"this is a test"})
+    requests.post(SLACK_URL, json={"text":"this *just* a test from smoke test trial"})
