d6aceeadb8f71e74ee25acfc3111528d0d23107f
Adam Shimali <adam.shimali@gmail.com>
2022-01-10 17:25:10 +0000

    json endpoint for for just dataset has been removed. The download is actually via entity object

--- tests/test_site.py
+++ tests/test_site.py
@@ -11,12 +11,3 @@ def test_get_healthcheck():
     data = resp.json()
 
     assert data["status"] == "OK"
-
-def test_get_json():
-    json_url = f"{BASE_URL}/dataset/local-authority-eng.json"
-    resp = requests.get(json_url)
-    resp.raise_for_status()
-    data = resp.json()
-
-    assert data["collection"] == "organisation"
-    assert data["dataset"] == "local-authority-eng"
